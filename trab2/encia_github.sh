b90343fd3e58e49685b0ab3208397e1509033453
BrunaToledoGuedes <brunatoledo30@gmail.com>
2017-06-11 11:59:46 -0400

    trab2

---
+++ trab2/encia_github.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git config --global user.name "BrunaToledoGuedes"
+git confic --global user.email "brunatoledo30@gmail.com"
+git add *
+git commit -m "trab2"
+git push
+
---
+++ trab2/envia_github.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global user.name "BrunaToledoGuedes"
+git config --global user.email "brunatoledo30@gmail.com"
+git add *
+git commit -m "$1"
+git push
+
